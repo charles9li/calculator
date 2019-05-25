43e24a7ba1e7e2e361f78ffc71067545b4a2f4a1
Charles Li <charles9li@berkeley.edu>
2019-05-25 09:03:47 -0700

    compressed select method

--- ivcalc/Data/SelectNatures.java
+++ ivcalc/Data/SelectNatures.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class SelectNatures {
 
@@ -22,48 +21,18 @@ public class SelectNatures {
         return conn;
     }
 
-    public void select() {
-        String sql = "SELECT * FROM natures";
-
-        try {
-            Connection conn = this.connect();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while (rs.next()) {
-                System.out.println(rs.getString("nature"));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
     public double select(String nature, String column) {
-        try {
-            ResultSet rs = result(nature);
-            return rs.getDouble(column);
-        } catch (SQLException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-        return 0.0;
-    }
-
-    private ResultSet result(String nature) {
         String sqlStatement = "SELECT * FROM natures WHERE nature = ?";
 
         try {
             Connection conn = this.connect();
             PreparedStatement preparedStatement = conn.prepareStatement(sqlStatement);
             preparedStatement.setString(1, nature);
-            return preparedStatement.executeQuery();
+            ResultSet rs = preparedStatement.executeQuery();
+            return rs.getDouble(column);
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            return null;
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
-    }
-
-    public static void main(String[] args) {
-        SelectNatures dn = new SelectNatures();
-        System.out.println(dn.select("Bold", "hp"));
+        return 0.0;
     }
 }
