327f02f8a8f6c6b7ef2deaf1e8704017be6bab9c
Charles Li <charles9li@berkeley.edu>
2019-05-25 09:11:53 -0700

    implements Select interface

--- ivcalc/Data/SelectNatures.java
+++ ivcalc/Data/SelectNatures.java
@@ -6,26 +6,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class SelectNatures {
+public class SelectNatures implements Select {
 
-    private Connection connect() {
-        Connection conn = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:C:./ivcalc/Data/natures.db");
-        } catch (SQLException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        } catch (ClassNotFoundException e) {
-            System.err.println("sqlite-JDBC driver not downloaded");
-        }
-        return conn;
-    }
+    private static final String DATABASE = "natures.db";
 
+    @Override
     public double select(String nature, String column) {
         String sqlStatement = "SELECT * FROM natures WHERE nature = ?";
 
         try {
-            Connection conn = this.connect();
+            Connection conn = connect(DATABASE);
             PreparedStatement preparedStatement = conn.prepareStatement(sqlStatement);
             preparedStatement.setString(1, nature);
             ResultSet rs = preparedStatement.executeQuery();
