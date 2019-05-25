5123986cbad3f4c7a9c3cded9f8dbd2f26dc13d8
Charles Li <charles9li@berkeley.edu>
2019-05-25 09:32:58 -0700

    added tests for SelectPokemon class

--- ivcalc/Test/TestSelect.java
+++ ivcalc/Test/TestSelect.java
@@ -1,5 +1,6 @@
 package ivcalc.Test;
 
+import ivcalc.Data.SelectPokemon;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -12,5 +13,12 @@ public class TestSelect {
         SelectNatures sn = new SelectNatures();
         assertEquals(0.9, sn.select("Bold", "atk"), 0.001);
         assertEquals(1.0, sn.select("Serious", "hp"), 0.001);
+    }
+
+    @Test
+    public void TestSelectPokemon() {
+        SelectPokemon sp = new SelectPokemon();
+        assertEquals(45, sp.select("Bulbasaur", "hp"));
+        assertEquals(108, sp.select("Terrakion", "spe"));
     }
 }
