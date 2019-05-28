f4ae6f5b609e438c56f7747f6f3514f188f13d91
Charles Li <charles9li@berkeley.edu>
2019-05-27 19:58:24 -0700

    installed javafx libraries and successfully carried out a sample

--- ivcalc/GUI/IVCalcGUI.java
+++ ivcalc/GUI/IVCalcGUI.java
@@ -1,4 +1,36 @@
 package ivcalc.GUI;
 
-public class IVCalcGUI {
+import ivcalc.Data.SelectPokemon;
+
+import javax.swing.*;
+
+public class IVCalcGUI extends JFrame {
+
+    private static final SelectPokemon sp = new SelectPokemon();
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Pokemon S/M IV Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLocation(430, 100);
+
+        JPanel panel = new JPanel();
+
+        frame.add(panel);
+
+        JLabel label = new JLabel("Pokemon");
+        label.setVisible(true);
+
+        panel.add(label);
+
+
+//        JComboBox<String> pokemonSearch = new JComboBox<>(sp.selectPokemonNames());
+        JComboBox<String> pokemonSearch = new JComboBox<>(new String[] {"", "Bulbasaur"});
+        pokemonSearch.setEditable(true);
+        panel.add(pokemonSearch);
+
+
+        panel.add(pokemonSearch);
+        frame.setVisible(true);
+    }
 }
