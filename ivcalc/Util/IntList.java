1985f302de3693b25f82f9261bffe5493c21ecd9
Charles Li <charles9li@berkeley.edu>
2019-06-21 11:32:17 -0700

    added comments

--- ivcalc/Util/IntList.java
+++ ivcalc/Util/IntList.java
@@ -7,8 +7,19 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Contains methods used to create ordered Lists of integers.
+ */
 public class IntList {
 
+    /**
+     * Creates a List instance containing integers between a lower and upper
+     * bound, inclusive.
+     *
+     * @param lowerBound lower bound
+     * @param upperBound upper bound
+     * @return List instance
+     */
     public static List<Integer> createIntList(int lowerBound, int upperBound) {
         List<Integer> returnList = new ArrayList<>();
         for (int iv = lowerBound; iv <= upperBound; iv++) {
@@ -17,6 +28,14 @@ public class IntList {
         return returnList;
     }
 
+    /**
+     * Takes a Deque containing Lists of integers and returns the intersection
+     * between them. If there is no intersection, then an empty List is
+     * returned.
+     *
+     * @param listDeque Deque containing Lists of integers.
+     * @return intersection of input integers Lists
+     */
     public static List<Integer> intListIntersect(Deque<List<Integer>> listDeque) {
         if (listDeque.isEmpty()) {
             throw new IllegalArgumentException("Deque must contain at least one List instance.");
