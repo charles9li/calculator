21ad82a40c3c9342fa8a3839a16ba9b01ee29123
Charles Li <charles9li@berkeley.edu>
2019-06-21 11:52:17 -0700

    fixed bug where evoIndex wasn't assigned and added comments

--- ivcalc/Calculator/LevelInfo.java
+++ ivcalc/Calculator/LevelInfo.java
@@ -5,25 +5,55 @@ import ivcalc.Util.StatType;
 
 import java.util.Map;
 
+/**
+ * Contains information from an input line.
+ */
 public class LevelInfo {
 
+    /**
+     * Pokemon level.
+     */
     private int level;
+
+    /**
+     * Indicates which evolution the input line corresponds to.
+     */
     private int evoIndex;
+
+    /**
+     * Inputted stats.
+     */
     private Map<StatType, Integer> stats;
+
+    /**
+     * Inputted effort values.
+     */
     private Map<StatType, Integer> effortValues;
 
-    public LevelInfo(int lvl, int evoIndex, int hpStat, int atkStat, int defStat, int spaStat, int spdStat, int speStat) {
-        this(lvl, evoIndex, hpStat, atkStat, defStat, spaStat, spdStat, speStat, 0, 0, 0, 0, 0, 0);
+    /**
+     * Constructor for no effort values.
+     */
+    public LevelInfo(int lvl, int evoIndex,
+                     int hpStat, int atkStat, int defStat,
+                     int spaStat, int spdStat, int speStat) {
+        this(lvl, evoIndex, hpStat, atkStat, defStat, spaStat, spdStat, speStat,
+            0, 0, 0, 0, 0, 0);
     }
 
-    public LevelInfo(int lvl, int evoIndex,
+    /**
+     * Constructor for the LevelInfo class.
+     */
+    public LevelInfo(int lvl, int evoInd,
                       int hpStat, int atkStat, int defStat,
                       int spaStat, int spdStat, int speStat,
                       int hpEV, int atkEV, int defEV,
                       int spaEV, int spdEV, int speEV) {
         level = lvl;
-        stats = StatMap.createStatMap(hpStat, atkStat, defStat, spaStat, spdStat, speStat);
-        effortValues = StatMap.createStatMap(hpEV, atkEV, defEV, spaEV, spdEV, speEV);
+        evoIndex = evoInd;
+        stats = StatMap.createStatMap(hpStat, atkStat, defStat,
+            spaStat, spdStat, speStat);
+        effortValues = StatMap.createStatMap(hpEV, atkEV, defEV,
+            spaEV, spdEV, speEV);
     }
 
     public int getLevel() {
